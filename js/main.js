9fb3a8647cb0e130db80c16189cb5dfe8b2bbc9e
Rocky Truong <Rockytruong19@gmail.com>
2021-06-12 00:44:51 +0000

    Show error message if Pokemon can't be found. #1

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var $searchBar = document.querySelector('#searchBar');
 var $detailName = document.querySelector('.detail-name');
 var $detailImg = document.querySelector('.detail-img');
 var $description = document.querySelector('.description');
+var $errorCode = document.querySelector('.error');
+var $viewPokemon = document.querySelector('#view-pokemon');
 
 $searchLink.addEventListener('click', handleSearch);
 $searchButton.addEventListener('click', handleSearch);
@@ -28,18 +30,28 @@ function getPokemonDetail(name) {
   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
+    if (xhr.status === 404) {
+      $errorCode.setAttribute('class', 'error view');
+      $searchBar.className = 'view';
+      $viewPokemon.className = 'view hidden';
+      return;
+    }
     var pokemonName = xhr.response.name.charAt(0).toUpperCase() + xhr.response.name.slice(1);
     $detailName.textContent = pokemonName;
     $detailImg.setAttribute('src', xhr.response.sprites.front_default);
   });
-
   var xhr2 = new XMLHttpRequest();
   xhr2.open('GET', 'https://pokeapi.co/api/v2/pokemon-species/' + name);
   xhr2.responseType = 'json';
   xhr2.addEventListener('load', function () {
-    $description.textContent = this.response.flavor_text_entries[0].flavor_text;
+    if (xhr2.status === 404) {
+      $errorCode.setAttribute('class', 'error view');
+      $searchBar.className = 'view';
+      $viewPokemon.className = 'view hidden';
+      return;
+    }
+    $description.textContent = this.response.flavor_text_entries[1].flavor_text;
   });
-
   xhr.send();
   xhr2.send();
 }
@@ -49,7 +61,6 @@ function handleSubmit(event) {
   $detailName.textContent = '';
   $detailImg.setAttribute('src', '');
   $description.textContent = '';
-  $searchBar.className = 'view hidden';
   getPokemonDetail($form.elements.name.value);
   $form.reset();
 
