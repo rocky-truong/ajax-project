f8ff09a68daff023c6bf272af82eb892f6494c9c
Rocky Truong <Rockytruong19@gmail.com>
2022-01-05 23:05:02 +0000

    Set up localStorage to store on data.js for when a Pokemon is saved.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,13 @@
 /* exported data */
+var favorites = [];
+var previousFavorites = localStorage.getItem('pokedata-local-storage');
+if (previousFavorites !== null) {
+  favorites = JSON.parse(previousFavorites);
+}
+
+window.addEventListener('beforeunload', jsonHandler);
+
+function jsonHandler(event) {
+  var favoritesJSON = JSON.stringify(favorites);
+  localStorage.setItem('pokedata-local-storage', favoritesJSON);
+}
